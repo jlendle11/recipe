93999f51521fc901a92e402f86d9b57a0c057172
Justin Lendle <lendlejustin@gmail.com>
2019-09-16 10:01:17 -0400

    test4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
-  # before_action :authorize_request, except: :create
-
-  before_action :set_user, only: %i[show update destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
   before_action :authorize_request, except: :create
+
+  # before_action :set_user, only: %i[show update destroy]
+  # before_action :authorize_request, except: :create
 
   # GET /users
   def index
